b34c4a4ce66f7ea3fb2549c75cdd2ecc24cc0c72
qinghongyu <708962513@qq.com>
2023-12-05 11:19:51 +0800

    asf

--- dconv2d.py
+++ dconv2d.py
@@ -26,16 +26,36 @@ class FeedBackNet(nn.Module):
         super(FeedBackNet, self).__init__()
         self.bn = nn.BatchNorm2d(16)
         self.preconv = nn.Conv2d(1, 16, 1, 1, 0)
-        self.conv1 = nn.Conv2d(16, 16, 3, 1, 1)
-        self.conv1a = nn.Conv2d(16, 16, 5, 1, 2)
-        self.conv2 = nn.Conv2d(16, 32, 3, 1, 1)
-        self.conv2a = nn.Conv2d(16, 32, 5, 1, 2)
-        self.conv3 = nn.Conv2d(32, 16, 5, 1, 2)
-        self.conv3a = nn.Conv2d(32, 16, 7, 1, 3)
+        # layers_1
+        self.conv1 = nn.Conv2d(16, 4, 3, 1, 1)
+        self.conv1a = nn.Conv2d(16, 4, 5, 1, 2)
+        self.conv1b = nn.Conv2d(16, 4, 7, 1, 3)
+        self.conv1c = nn.Conv2d(16, 4, 9, 1, 4)
+        # layers_2
+        self.conv2 = nn.Conv2d(16, 8, 3, 1, 1)
+        self.conv2a = nn.Conv2d(16, 8, 5, 1, 2)
+        self.conv2b = nn.Conv2d(16, 8, 7, 1, 3)
+        self.conv2c = nn.Conv2d(16, 8, 9, 1, 4)
+        # layers_3
+        self.conv3 = nn.Conv2d(32, 16, 3, 1, 1)
+        self.conv3a = nn.Conv2d(32, 16, 5, 1, 2)
+        self.conv3b = nn.Conv2d(32, 16, 7, 1, 3)
+        self.conv3c = nn.Conv2d(32, 16, 9, 1, 4)
+        # layers_4
+        self.conv4 = nn.Conv2d(64, 8, 3, 1, 1)
+        self.conv4a = nn.Conv2d(64, 8, 5, 1, 2)
+        self.conv4b = nn.Conv2d(64, 8, 7, 1, 3)
+        self.conv4c = nn.Conv2d(64, 8, 9, 1, 4)
+        #layers_5
+        self.conv5 = nn.Conv2d(32, 4, 3, 1, 1)
+        self.conv5a = nn.Conv2d(32, 4, 5, 1, 2)
+        self.conv5b = nn.Conv2d(32, 4, 7, 1, 3)
+        self.conv5c = nn.Conv2d(32, 4, 9, 1, 4)
         self.sa = SpatialAttention()
-        self.conv4 = nn.Conv2d(16, 8, 3, 2, 0)
-        self.conv5 = nn.Conv2d(8, 4, 3, 2, 0)
-        self.back = nn.Sequential(self.conv4, self.conv5)
+        self.conv_back1 = nn.Conv2d(16, 16, 5, 2, 0)
+        self.conv_back2 = nn.Conv2d(16, 16, 3, 2, 0)
+        self.back = nn.Sequential(self.conv_back1, self.conv_back2)
+        self.sn1 = neuron.ParametricLIFNode(surrogate_function=surrogate.ATan(), detach_reset=True)
 
     def forward(self, x):
         x = self.preconv(x)
@@ -43,18 +63,35 @@ class FeedBackNet(nn.Module):
         for i in range(4):
             a1 = self.conv1(input)
             a2 = self.conv1a(input)
-            a3 = a1 + a2
-            b1 = self.conv2(a3)
-            b2 = self.conv2a(a3)
-            b3 = b1 + b2
-            c1 = self.conv3(b3)
-            c2 = self.conv3a(b3)
-            c3 = c1 + c2
-            c3 = self.bn(c3)
-            input = x + c3
-        sa = self.sa(c3)
-        output = sa * c3
-        output=self.back(output)
+            a3=self.conv1b(input)
+            a4=self.conv1c(input)
+            a5=torch.cat((a1,a2,a3,a4),dim=1)
+            b1 = self.conv2(a5)
+            b2 = self.conv2a(a5)
+            b3 = self.conv2b(a5)
+            b4=self.conv2c(a5)
+            b5=torch.cat((b1,b2,b3,b4),dim=1)
+            c1 = self.conv3(b5)
+            c2 = self.conv3a(b5)
+            c3 = self.conv3b(b5)
+            c4 = self.conv3c(b5)
+            c5=torch.cat((c1,c2,c3,c4),dim=1)
+            d1 = self.conv4(c5)
+            d2 = self.conv4a(c5)
+            d3 = self.conv4b(c5)
+            d4 = self.conv4c(c5)
+            d5=torch.cat((d1,d2,d3,d4),dim=1)
+            e1 = self.conv5(d5)
+            e2 = self.conv5a(d5)
+            e3 = self.conv5b(d5)
+            e4 = self.conv5c(d5)
+            e5=torch.cat((e1,e2,e3,e4),dim=1)
+            e5 = self.bn(e5)
+            e5=self.sn1(e5)
+            input = x + e5
+        sa = self.sa(e5)
+        output = sa * e5
+        output = self.back(output)
         return output
 
 
@@ -70,11 +107,13 @@ class ConvBlock(nn.Module):
                                     dilation=dilation)
         self.conv2 = nn.Conv2d(n_inputs, n_outputs, kernel_size=1)
         self.net2 = nn.Sequential(self.maxpool, self.conv2)
+        self.sa = SpatialAttention()
 
     def forward(self, x):
         out = self.net(x)
         res = self.net2(x)
         out = out + res
+        out = out * self.sa(out)
         out = self.sn1(out)
         return out
 
@@ -104,9 +143,9 @@ class BackEndNet(nn.Module):
         self.convnet = ConvNet()
         self.flat = nn.Flatten()
         # self.lin1=nn.Linear(36*12*41,12*41)
-        self.lin1 = nn.Linear(1560, 1024)
+        self.lin1 = nn.Linear(6240, 1560)
         self.sn1 = neuron.ParametricLIFNode(surrogate_function=surrogate.ATan(), detach_reset=True)
-        self.lin2 = nn.Linear(1024, 12 * 41)
+        self.lin2 = nn.Linear(1560, 12 * 41)
         self.sn2 = neuron.ParametricLIFNode(surrogate_function=surrogate.ATan(), detach_reset=True)
         self.lin3 = nn.Linear(12 * 41, 35)
         self.sn3 = neuron.ParametricLIFNode(surrogate_function=surrogate.ATan(), detach_reset=True)
